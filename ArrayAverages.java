3c9b90637c3a4be35d8cafbe35ccff0040f3753b
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-02-11 21:44:04 +0000

    lemon

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -18,6 +18,7 @@ public class ArrayAverages{
       for(int col = 0; col < ray[row].length; col++)
       {
         sum+=col;
+        
       }
 
       rowAvg=sum/ray[row].length;
@@ -33,12 +34,46 @@ public class ArrayAverages{
     //top down and left to right
 
     //reverse of what we wrote yesterday
+    System.out.println("");
+    double colAvg = 0;
+    int colSum = 0;
+    int rowNum = 0;
+    
+    for(int col = 0; col < ray[0].length; col++)
+    {
+      rowNum = 0;
 
+      for(int row = 0; row < ray.length; row++)
+      {
+        rowNum+=col;
+      }
+      colSum += 1;
+      colAvg = rowNum;
+      System.out.println("The average of column " + col + " is: " + colAvg);
+    }
   }
 
   public void arrayAverage()
   {
+    System.out.println("");
 
+    int total = 0;
+    int cnt = 0;
+    double arrayAvg = 0;
+    
+
+    for(int row = 0; row<ray.length; row++)
+    {
+
+      for(int col = 0; col < ray[row].length; col++)
+      {
+        
+        total+=col;
+        cnt++;
+      }
+      arrayAvg = total/cnt;
+    }
+    System.out.println("The average of the array is: " + arrayAvg);
   }
 
   public String toString(){
