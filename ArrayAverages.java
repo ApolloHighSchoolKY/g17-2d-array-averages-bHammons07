c389319137f72ec241091ac919e3d0e56ee797b2
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-02-11 15:30:20 +0000

    lime

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -8,15 +8,36 @@ public class ArrayAverages{
     ray = rayray;
   }
 
-  public void rowAverages(){
-    
+  public void rowAverages()
+  {
+    double rowAvg = 0;
+    int sum = 0;
+    for(int row = 0; row < ray.length; row++)
+    {
+      //sum = 0;
+      for(int col = 0; col < ray[row].length; col++)
+      {
+        sum+=col;
+      }
+
+      rowAvg=sum/ray[row].length;
+
+      System.out.println("The average of row " + row + " is: " + rowAvg);
+    }
   }
 
-  public void columnAverages(){
+  public void columnAverages()
+  {
+    //column major order
+    //take each column and go through all the rows
+    //top down and left to right
+
+    //reverse of what we wrote yesterday
 
   }
 
-  public void arrayAverage(){
+  public void arrayAverage()
+  {
 
   }
 
@@ -25,5 +46,7 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+
+    return "";
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,21 @@
 public class Gee17{
   public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
 
+    int[][] ray = new int[7][7];
+
+    int seq = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col < ray[row].length; col++)
+      {
+        seq++;
+        ray[row][col]+=seq;
+      }
+    }
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
+
+    System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
